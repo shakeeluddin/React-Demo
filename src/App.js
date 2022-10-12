8567bea53587804eceaa4c17e95ad10e7565ed70
shakeeluddin <s_uddin91@hotmail.com>
2022-10-12 16:34:23 +0100

    to github with ya

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Try adding some html in here:</h1>
+      <p>Why is edge so terrible?</p>
+      <p>seems good to me</p>
+      <p>possibly just hadn't saved it?</p>
     </div>
-  );
+  )
 }
-
 export default App;
